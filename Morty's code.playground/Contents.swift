41c5e72188037a6c937ab30be1bc0b98690c8842
Brian Collings <LimpLion@Brians-MacBook-Pro.local>
2017-02-16 15:34:28 -0500

    my first commit

---
+++ Morty's code.playground/Contents.swift
@@ -0,0 +1,11 @@
+
+var BirthYear = 2000
+
+if BirthYear <= 1999 {
+    print("You are old enough to watch porn")
+    print("You were born in the 20th century")
+} else {
+    print("You are too young for this shit")
+    print("You were born in the 21st century (maybe)")
+}
+
